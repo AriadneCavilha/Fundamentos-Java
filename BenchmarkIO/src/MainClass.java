f1bd3e63bb707baaeb60f169589eb2543e6e6dd7
Ariadne Cavilha Jorge <ariadne.jorge@teclogica.com.br>
2026-05-25 20:13:40 -0300

    feat: Criação do projeto de leitura dos arquivos e faz comparação

---
+++ BenchmarkIO/src/MainClass.java
@@ -0,0 +1,8 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Path path = Paths.get("benchmark.txt");
+    }
+}
